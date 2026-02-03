2ccc8813872e831346c952698216dbe0c4cf7756
Vladimir Timofeev <v.timofeev1977@ya.ru>
2026-02-03 17:13:58 +0300

    edit Actions

--- src/test/scala/otus/Actions.scala
+++ src/test/scala/otus/Actions.scala
@@ -5,20 +5,27 @@ import io.gatling.http.Predef._
 import io.gatling.http.request.builder.HttpRequestBuilder
 
 object Actions {
-  val webTours: HttpRequestBuilder = http("webTours")
+  val getWebTours: HttpRequestBuilder = http("getWebTours")
     .get("webtours/")
     .check(status is 200)
 
-  val loginPage: HttpRequestBuilder = http("login")
-    .get("cgi-bin/nav.pl?in=home")
+  val getLoginPage: HttpRequestBuilder = http("getLoginPage")
+    .get("cgi-bin/nav.pl")
+    .queryParam("in", "home")
     .check(status is 200)
+    .check(substring(userSession => "value").saveAs("userSession"))
+
+  val postLogin: HttpRequestBuilder = http("postLogin")
+    .post("cgi-bin/login.pl")
 
   val homePage: HttpRequestBuilder = http("homePage")
-    .get("cgi-bin/login.pl?intro=true")
+    .get("cgi-bin/login.pl")
+    .queryParam("ntro", "true")
     .check(status is 200)
 
   val selectCity: HttpRequestBuilder = http("selectCity")
-    .get("cgi-bin/welcome.pl?page=search")
+    .get("cgi-bin/welcome.pl")
+    .queryParam("page", "search")
     .check(status is 200)
 
   val selectFlight: HttpRequestBuilder = http("selectFlight")
